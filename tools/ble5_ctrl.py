54fb2288e8a6178980d9ee461a38aac29dc63b8f
susabolca <susabolca@gmail.com>
2024-04-27 17:55:45 +0800

    fix: python control

--- tools/ble5_ctrl.py
+++ tools/ble5_ctrl.py
@@ -1,10 +1,31 @@
 import asyncio
 from bleak import BleakScanner
 
+devMap = {}
+
 async def main():
     devices = await BleakScanner.discover()
     for d in devices:
         print(d, d.metadata)
     BleakScanner.discovered_devices_and_advertisement_data = True
 
-asyncio.run(main())
+async def scan():
+    stop_event = asyncio.Event()
+
+    def callback(device, advertising_data):
+        if device is not None and device.name is not None and device.name.startswith("C26_"):
+            if device.name not in devMap:
+                devMap[device.name] = device
+                print(device, advertising_data)
+                #stop_event.set()
+
+    async with BleakScanner(callback) as scanner:
+        await asyncio.wait_for(stop_event.wait(), timeout=3)
+
+for i in range(10):
+    try:
+        asyncio.run(scan())
+    except Exception as e:
+        pass
+
+print(devMap)
